c62aa3088275d75c66aa1153433037242499e4dd
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-03-04 15:04:49 -0600

    Finished servlet exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
 
+//sets up our servelet to be at /hello-world
+@WebServlet (name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        System.out.println(req.getParameter("name"));
+        if(req.getParameter("name").isEmpty()){
+            out.println("<h1>Hello, World!</h1>");
+        }else{
+            out.println("<h1>Hello, " + req.getParameter("name") + "</h1>");
+        }
+        
     }
 }
